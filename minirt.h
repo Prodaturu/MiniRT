570d9555f2d0f991b54b5f29ee32a69ee8dd51a0
Saikiran Prodaturu <sprodaturu@gmail.com>
2024-06-27 15:51:21 +0200

    init for files -> minirt.h

---
+++ minirt.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 15:39:00 by sprodatu          #+#    #+#             */
+/*   Updated: 2024/06/27 15:49:27 by sprodatu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include "minirt.h"
+# include <math.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdio.h>
+# include <errno.h>
+# include <stdbool.h>
+
+#endif
