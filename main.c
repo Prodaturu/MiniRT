ef84cf49cda1d9a5b2a5bbed78aa8fdb2a94620b
trosinski08 <trosinski08@gmail.com>
2024-06-28 02:25:19 +0200

    ruszyla_maszyna

--- main.c
+++ main.c
@@ -6,36 +6,77 @@
 /*   By: trosinsk <trosinsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 14:21:02 by sprodatu          #+#    #+#             */
-/*   Updated: 2024/06/28 01:09:44 by trosinsk         ###   ########.fr       */
+/*   Updated: 2024/06/28 02:24:24 by trosinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+//for every aim function 1st step-split line
+//2nd step-check syntax
+//3rd step-create struct
+//4th step-create object, check how long is element, if have 3 or 4 elements
+int	parse_line(char *line, t_main_rt *main_rt)
+{
+	if (line[0] == 'A' && line[1] == ' ')
+		return (parse_ambient(line, main_rt));
+	else if (line[0] == 'C' && line[1] == ' ')
+		return (parse_camera(line, main_rt));
+	else if (line[0] == 'L' && line[1] == ' ')
+		return (parse_light(line, main_rt));
+	else if (line[0] == 's' && line[1] == 'p')
+		return (parse_sphere(line, main_rt));
+	else if (line[0] == 'p' && line[1] == 'l')
+		return (parse_plane(line, main_rt));
+	else if (line[0] == 'c' && line[1] == 'y')
+		return (parse_cylinder(line, main_rt));
+	else
+		return (0);
+}
+
+int	parser(int fd, t_main_rt *main_rt)
+{
+	t_main_rt	*main_rt;
+	char		*line;
+
+	main_rt = (t_main_rt *)malloc(sizeof(t_main_rt));
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (parse_line(line, main_rt))
+			line = get_next_line(fd);
+		else
+			return (ft_putendl_fd("Error: parsing error", 2), 1);
+	}
+	{
+		//check syntax
+		//create structs
+		//create objects
+		//render
+		// close(fd);
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	char	*line;
-	int		fd;
+	int			fd;
+	t_main_rt	*main_rt;
 
 	if (argc != 2)
 		return (ft_putendl_fd("Error: provide .rt file", 2), 1);
-	printf("%s\n", argv[1]);
 	if (ft_strncmp(ft_strrchr(argv[1], '.'), ".rt", 3))
 		return (ft_putendl_fd("Error: provide .rt file", 2), 1);
-	printf("File is .rt\n");
-	fd = open(argv[1], O_RDONLY);
-	printf("File descriptor: %d\n", fd);
-	line = get_next_line(fd);
-	printf("%s\n", line);
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0)
+			return (ft_putendl_fd("Error: file not found", 2), 1);
+		else
+			return (parser(fd, main_rt));
+	}
+	// create_image(main_rt); // to do
+	// mlx_loop(main_rt->mlx->mlx);
+	//do_your_math(main_rt); // to do
+	// free_rt(main_rt);
 	return (0);
 }
-/*
-check argc
-check argv[1] for .rt extension
-parse file with gnl and split by spaces
-check for elements, some cannot be missing some cannot duplicate
-check syntax for each element
-create a struct for each element
-create a struct for each object
-
-*/
--- minirt.h
+++ minirt.h
@@ -6,7 +6,7 @@
 /*   By: trosinsk <trosinsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 01:29:10 by trosinsk          #+#    #+#             */
-/*   Updated: 2024/06/28 01:10:05 by trosinsk         ###   ########.fr       */
+/*   Updated: 2024/06/28 02:12:31 by trosinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,67 @@
 # include <string.h>
 # include <math.h>
 
+# define PI 3.14159265358979323846
+
+typedef struct main_rt
+{
+	int				res_x;
+	int				res_y;
+	int				fd;
+	t_mlx			*mlx;
+	t_amb_rt		*amb;
+	t_cam_rt		*cam;
+	t_light_rt		*light;
+	t_sphere_rt		*sphere;
+	t_plane_rt		*plane;
+	t_cylinder_rt	*cylinder;
+	t_color_rt		*color;
+}				t_main_rt;
+
+typedef struct amb_rt
+{
+	int				counter;
+	double			ratio;
+	t_color_rt		*color;
+}				t_amb_rt;
+
+typedef struct cam_rt
+{
+	int				counter;
+	t_pov_rt		*pov;
+	t_vec_rt		*vec;
+	double			fov;
+}				t_cam_rt;
+
+typedef struct light_rt
+{
+	int				counter;
+	t_coord_rt		*coord;
+	double			ratio;
+	t_color_rt		*color;
+}				t_light_rt;
+
+typedef struct sphere_rt
+{
+	t_center_rt		*center;
+	double			diameter;
+	t_color_rt		*color;
+}				t_sphere_rt;
+
+
+typedef struct plane_rt
+{
+	t_coord_rt		*coord;
+	t_vec_rt		*vec;
+	t_color_rt		*color;
+}				t_plane_rt;
+
+typedef struct cylinder_rt
+{
+	t_center_rt		*center;
+	t_vec_rt		*vec;
+	double			diameter;
+	double			height;
+	t_color_rt		*color;
+}				t_cylinder_rt;
 #endif
