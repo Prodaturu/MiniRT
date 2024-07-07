14c17404511c17ad28d8b0979143187c55b9e202
Saikiran Prodaturu <sprodaturu@gmail.com>
2024-07-07 16:37:28 +0200

    modified main, modularized argument check with a function named argument_check

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: trosinsk <trosinsk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 14:21:02 by sprodatu          #+#    #+#             */
-/*   Updated: 2024/07/07 01:35:44 by trosinsk         ###   ########.fr       */
+/*   Updated: 2024/07/07 16:36:16 by sprodatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,16 @@ t_main_rt	*open_and_init(int *fd, t_main_rt *main_rt, char **argv)
 	return (main_rt);
 }
 
+void	argument_check(int argc, char **argv, t_main_rt *main_rt, int fd)
+{
+	if (argc != 2)
+		return (ft_putendl_fd("Error: path to file expected", 2), 1);
+	else if (ft_strncmp(ft_strrchr(argv[1], '.'), ".rt", 3))
+		return (ft_putendl_fd("Error: provide .rt file", 2), 1);
+	else
+		main_rt = open_and_init(&fd, main_rt, argv);
+}
+
 int	main(int argc, char **argv)
 {
 	int			fd;
@@ -45,15 +55,10 @@ int	main(int argc, char **argv)
 
 	main_rt = NULL;
 	fd = 0;
-	if (argc != 2)
-		return (ft_putendl_fd("Error: path to file expected", 2), 1);
-	else if (ft_strncmp(ft_strrchr(argv[1], '.'), ".rt", 3))
-		return (ft_putendl_fd("Error: provide .rt file", 2), 1);
-	else
-		main_rt = open_and_init(&fd, main_rt, argv);
+	argument_check(argc, argv, main_rt, &fd);
 	if (!parser(fd, main_rt))
 		mlx = create_image(main_rt);
-		// post_parsing(main_rt); 
+		// post_parsing(main_rt);
 	else
 		return (ft_putendl_fd("Error: parsing error", 2), 1);
 	return (0);
@@ -64,5 +69,4 @@ to implement calculation colors in thread for each channel I need
 how you want to calculate the color of the pixel (how you check inside
 the sphere, plane, cylinder, etc. if the ray intersects with the object)
 then I can build the logic when and how to change values in the image
-
 */
