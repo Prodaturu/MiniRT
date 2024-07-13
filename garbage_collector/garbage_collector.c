035445373e828af778378503eaec1d6682d83795
Saikiran Prodaturu <sprodaturu@gmail.com>
2024-07-14 00:42:11 +0200

    Garbage collector added; Functions to init, display nodes, add, free a specific node done; TODO: function to free all nodes in the garbage collector

--- garbage_collector/garbage_collector.c
+++ garbage_collector/garbage_collector.c
@@ -6,7 +6,92 @@
 /*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 20:37:37 by sprodatu          #+#    #+#             */
-/*   Updated: 2024/07/13 20:37:38 by sprodatu         ###   ########.fr       */
+/*   Updated: 2024/07/14 00:32:44 by sprodatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/garbage_collector.h"
+
+void	add_to_garb_col(t_garbage *garb_col, void *ptr)
+{
+	t_garb_node	*new_node;
+
+	if (!ptr)
+		return ;
+	new_node = malloc(sizeof(t_garb_node));
+	new_node->ptr_data = ptr;
+	if (!new_node)
+		return (ft_putendl_fd("Error: Malloc Failure!", 2), exit(1));
+	if (garb_col->length == 0)
+	{
+		garb_col->length = 1;
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		garb_col->head = new_node;
+		garb_col->tail = new_node;
+	}
+	else
+	{
+		new_node->prev = garb_col->tail;
+		new_node->next = NULL;
+		garb_col->tail->next = new_node;
+		garb_col->tail = new_node;
+		garb_col->length++;
+	}
+	return ;
+}
+
+void	free_specific_alloc(t_garbage *garb_col, void *ptr)
+{
+	t_garb_node	*current;
+	t_garb_node	*prev;
+
+	current = garb_col->head;
+	while (current)
+	{
+		if (current->ptr_data == ptr)
+		{
+			free(current->ptr_data);
+			if (prev == NULL)
+				garb_col->head = current->next;
+			else
+				prev->next = current->next;
+			free(current);
+			return ;
+		}
+		prev = current;
+		current = current->next;
+	}
+}
+
+int	display_allocations(t_garbage *garb_col)
+{
+	t_garb_node	*current;
+	int			i;
+
+	current = garb_col->head;
+	printf("Current allocations:\n");
+	while (current)
+	{
+		printf("the pointer at indexed %d points to : \"%p\"\n",
+			current->ptr_data);
+		current = current->next;
+		i++;
+	}
+}
+
+t_garbage	*garbage_collector_init(void)
+{
+	t_garbage	*garb_col;
+
+	garb_col = (t_garbage *)malloc(sizeof(t_garbage));
+	if (!garb_col)
+	{
+		ft_putendl_fd("Error: garbage collector initialization failed", 2);
+		exit(1);
+	}
+	garb_col->length = 0;
+	garb_col->head = NULL;
+	garb_col->tail = NULL;
+	return (garb_col);
+}
--- include/garbage_collector.h
+++ include/garbage_collector.h
@@ -6,7 +6,7 @@
 /*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 20:40:33 by sprodatu          #+#    #+#             */
-/*   Updated: 2024/07/13 20:57:43 by sprodatu         ###   ########.fr       */
+/*   Updated: 2024/07/13 23:30:50 by sprodatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,23 +31,61 @@
  */
 typedef struct s_garb_node
 {
-	void				*data;
+	void				*ptr_data;
 	struct s_garb_node	*next;
 	struct s_garb_node	*prev;
-}			t_garb_node;
+}						t_garb_node;
 
 /**
  * @brief struct to store the garbage collector data
  * 
- * @param head pointer to the head of the garbage collector
- * @param tail pointer to the tail of the garbage collector
- * @param length length of the garbage collector
+ * @param length (size_t) : length of the garbage collector
+ * @param head (t_garb_node) pointer to head node of garbage collector
+ * @param tail (t_garb_node) pointer to tail node of garbage collector
  */
 typedef struct s_garbage
 {
+	size_t			length;
 	t_garb_node		*head;
 	t_garb_node		*tail;
-	size_t			length;
-}			t_garbage;
+}					t_garbage;
+
+//* --- --- --- garbage_collector.c --- --- --- *//
+
+/**
+ * @brief Frees a specific node / allocation from the garbage collector
+ *
+ * This fuction is usefull when we need to free a specific node
+ * after using it instead of keeping it in the garbage collector
+ * till the end of program
+ * 
+ * @param garb_col pointer to the garbage collector
+ * @param ptr pointer to the adress of the data to be freed
+ */
+void			free_specific_alloc(t_garbage *garb_col, void *ptr);
+
+/**
+ * @brief Displays all the allocations passed to the garbage 
+ * collector in the order they were added
+ * 
+ * @param garb_col pointer to the garbage collector
+ * @return int 0 if successful
+ * 
+ * @note This function is used for debugging purposes
+ */
+int				display_allocations(t_garbage *garb_col);
+
+/**
+ * @brief Initializes the first node of the garbage collector
+ * and adds the pointer to the garbage collector
+ * 
+ * @param garb_col pointer to the garbage collector
+ * @param ptr pointer to the data to be stored
+ * @return t_garb_node* pointer to nodes of the garbage collector
+ * 
+ * @note This function needs to be called before any other to 
+ * be able to use the garbage collector
+ */
+t_garbage		*garbage_collector_init(void);
 
 #endif
