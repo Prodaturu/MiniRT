abed1ae4ce37abbb8dad789c9246f45934d97860
Saikiran Prodaturu <sprodaturu@gmail.com>
2024-07-14 03:42:44 +0200

    restructured functions adding to garbage collector and freeing from garbage collector into two different files for more modularity

--- garbage_collector/garbage_collector.c
+++ garbage_collector/garbage_collector.c
@@ -6,41 +6,12 @@
 /*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 20:37:37 by sprodatu          #+#    #+#             */
-/*   Updated: 2024/07/14 03:37:24 by sprodatu         ###   ########.fr       */
+/*   Updated: 2024/07/14 03:41:06 by sprodatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/garbage_collector.h"
 
-void	free_node(t_garb_node *node)
-{
-	if (!node)
-		return ;
-	free(node);
-	node = NULL;
-}
-
-void	free_garbage(t_garbage *garb_col)
-{
-	t_garb_node	*current;
-	t_garb_node	*next;
-
-	if (garb_col->length == 0)
-		return ;
-	current = garb_col->tail;
-	while (current)
-	{
-		garb_col->length--;
-		if (current->ptr_data != NULL)
-			free_node(current->ptr_data);
-		next = current;
-		current = current->prev;
-		free_node(next);
-	}
-	if (garb_col)
-		free_node(garb_col);
-	return ;
-}
 
 void	add_to_garb_col(t_garbage *garb_col, void *ptr)
 {
@@ -67,29 +38,6 @@ void	add_to_garb_col(t_garbage *garb_col, void *ptr)
 	garb_col->tail = new_node;
 	garb_col->length++;
 	return ;
-}
-
-void	free_specific_alloc(t_garbage *garb_col, void *ptr)
-{
-	t_garb_node	*current;
-	t_garb_node	*prev;
-
-	current = garb_col->head;
-	while (current)
-	{
-		if (current->ptr_data == ptr)
-		{
-			free(current->ptr_data);
-			if (prev == NULL)
-				garb_col->head = current->next;
-			else
-				prev->next = current->next;
-			free(current);
-			return ;
-		}
-		prev = current;
-		current = current->next;
-	}
 }
 
 int	display_allocations(t_garbage *garb_col)
--- garbage_collector/garbage_remover.c
+++ garbage_collector/garbage_remover.c
@@ -6,8 +6,61 @@
 /*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 03:37:57 by sprodatu          #+#    #+#             */
-/*   Updated: 2024/07/14 03:38:47 by sprodatu         ###   ########.fr       */
+/*   Updated: 2024/07/14 03:41:46 by sprodatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/garbage_collector.h"
+
+void	free_node(t_garb_node *node)
+{
+	if (!node)
+		return ;
+	free(node);
+	node = NULL;
+}
+
+void	free_garbage(t_garbage *garb_col)
+{
+	t_garb_node	*current;
+	t_garb_node	*next;
+
+	if (garb_col->length == 0)
+		return ;
+	current = garb_col->tail;
+	while (current)
+	{
+		garb_col->length--;
+		if (current->ptr_data != NULL)
+			free_node(current->ptr_data);
+		next = current;
+		current = current->prev;
+		free_node(next);
+	}
+	if (garb_col)
+		free_node(garb_col);
+	return ;
+}
+
+void	free_specific_alloc(t_garbage *garb_col, void *ptr)
+{
+	t_garb_node	*current;
+	t_garb_node	*prev;
+
+	current = garb_col->head;
+	while (current)
+	{
+		if (current->ptr_data == ptr)
+		{
+			free(current->ptr_data);
+			if (prev == NULL)
+				garb_col->head = current->next;
+			else
+				prev->next = current->next;
+			free(current);
+			return ;
+		}
+		prev = current;
+		current = current->next;
+	}
+}
