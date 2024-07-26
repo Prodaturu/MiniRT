ade9e665ec1044e86f106f3ae2ac99730b08c88c
Saikiran Prodaturu <sprodaturu@gmail.com>
2024-07-26 07:42:14 +0200

    improvements to file checker

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 14:21:02 by sprodatu          #+#    #+#             */
-/*   Updated: 2024/07/25 23:00:36 by sprodatu         ###   ########.fr       */
+/*   Updated: 2024/07/26 07:41:54 by sprodatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,30 +39,42 @@ void	file_checker(int argc, char **argv)
 		|| (ft_strlen(file_type) != 3))
 	{
 		ft_putendl_fd("Error: provide a .rt file", 2);
-		printf(RED "provided: %s file, use .rt file\n" RESET, file_type);
+		if (file_type)
+			printf(RED "Provided: %s file, use .rt file\n" RESET, file_type);
+		else
+			printf(RED "File type not found, use .rt file\n" RESET);
 		exit(1);
 	}
 	if (DEBUG)
 		printf(GREEN "File type is: %s\n" RESET, file_type);
 }
 
+// int	main(int argc, char **argv)
+// {
+// 	t_garbage	*garb_col;
+// 	t_main_rt	*main_rt;
+
+// 	file_checker(argc, argv);
+// 	garb_col = garbage_collector_init();
+// 	main_rt = (t_main_rt *)malloc(sizeof(t_main_rt));
+// 	if (!main_rt)
+// 		return (ft_putendl_fd("Error: malloc error", 2), 1);
+// 	main_rt_init(main_rt, garb_col);
+// 	parser(garb_col, argv[1], main_rt);
+// 	init_scene_struct(main_rt, garb_col);
+// 	// printf("DEBUG!!\n");
+// 	renderer(main_rt);
+// 	free_garbage(garb_col);
+// 	free(garb_col);
+// 	return (0);
+// }
+
 int	main(int argc, char **argv)
 {
-	t_garbage	*garb_col;
-	t_main_rt	*main_rt;
+	// t_garbage	*gc;
+	// t_main_rt	*rt;
 
 	file_checker(argc, argv);
-	garb_col = garbage_collector_init();
-	main_rt = (t_main_rt *)malloc(sizeof(t_main_rt));
-	if (!main_rt)
-		return (ft_putendl_fd("Error: malloc error", 2), 1);
-	main_rt_init(main_rt, garb_col);
-	parser(garb_col, argv[1], main_rt);
-	init_scene_struct(main_rt, garb_col);
-	printf("DEBUG!!\n");
-	renderer(main_rt);
-	free_garbage(garb_col);
-	free(garb_col);
 	return (0);
 }
 
